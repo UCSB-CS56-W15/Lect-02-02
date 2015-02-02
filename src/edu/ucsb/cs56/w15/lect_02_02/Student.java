5646128b91aa4825433ecfc5dc07573a65fccc88
Phill Conrad <pconrad@cs.ucsb.edu>
2015-02-02 10:19:47 -0800

    WIP haven't compiled yet

---
+++ src/edu/ucsb/cs56/w15/lect_02_02/Student.java
@@ -0,0 +1,6 @@
+package edu.ucsb.cs56.w15.lect_02_02;
+
+public class Student {
+
+
+}
---
+++ src/edu/ucsb/cs56/w15/lect_02_02/TestDriver.java
@@ -0,0 +1,9 @@
+package edu.ucsb.cs56.w15.lect_02_02;
+
+
+public class TestDriver {
+
+    public static void main(String [] args) {
+	System.out.println("Yo!  Fill me in later");
+    }
+}
